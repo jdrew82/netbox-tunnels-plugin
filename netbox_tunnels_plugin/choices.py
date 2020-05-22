d0c16d05e6d79bce7ff53ca6eb0ca9aad383b8e9
Justin Drew <jdrew82@users.noreply.github.com>
2020-05-21 21:34:52 -0500

    Add configuration for tunnel statuses

---
+++ netbox_tunnels_plugin/choices.py
@@ -0,0 +1,17 @@
+from utilities.choices import ChoiceSet
+
+
+class TunnelStatusChoices(ChoiceSet):
+    """List of possible status for a Tunnel."""
+
+    STATUS_PENDING_CONFIGURATION = 'pending-configuration'
+    STATUS_CONFIGURED = 'configured'
+    STATUS_PENDING_DELETION = 'pending-deletion'
+    STATUS_CONFIGURATION_ERROR = 'configuration-error'
+
+    CHOICES = (
+        (STATUS_PENDING_CONFIGURATION, 'Pending Configuration'),
+        (STATUS_CONFIGURED, 'Configured'),
+        (STATUS_PENDING_DELETION, 'Pending Deletion'),
+        (STATUS_CONFIGURATION_ERROR, 'Configuration Error'),
+    )
