bc4163d4078558349f4237e2780deae8e0f74933
Justin Drew <jdrew82@users.noreply.github.com>
2020-05-27 17:57:32 -0500

    Add url handling

---
+++ netbox_tunnels/urls.py
@@ -0,0 +1,27 @@
+"""Django urlpatterns declaration for netbox_tunnels plugin.
+(c) 2020 Justin Drew
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from django.urls import path
+
+from .views import (
+    TunnelListView,
+    TunnelCreateView,
+    # TunnelBulkDeleteView,
+    # TunnelFeedBulkImportView,
+)
+
+urlpatterns = [
+    path("", TunnelListView.as_view(), name="tunnels_list"),
+    path("add/", TunnelCreateView.as_view(), name="tunnels_creation"),
+    # path("delete/", TunnelBulkDeleteView.as_view(), name="tunnels_bulk_delete"),
+    # path("import/", TunnelFeedBulkImportView.as_view(), name="tunnels_import"),
+]
