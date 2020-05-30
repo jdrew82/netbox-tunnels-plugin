fa08d1be68033a64b1411299c479e026cda8f80f
Justin Drew <jdrew82@users.noreply.github.com>
2020-05-29 21:28:45 -0500

    Update min version and verbose name

--- netbox_tunnels/__init__.py
+++ netbox_tunnels/__init__.py
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+__version__ = "0.2.3"
+
 from extras.plugins import PluginConfig
 
 
@@ -18,12 +20,13 @@ class TunnelsConfig(PluginConfig):
     """This class defines attributes for the NetBox Tunnels Plugin."""
 
     name = "netbox_tunnels"
-    verbose_name = "Tunnels"
-    description = "Netbox Tunnels"
-    version = "0.2.3"
-    base_url = "tunnels"
+    verbose_name = "Network Tunnels"
+    version = __version__
+    description = "Network Tunnels"
+    base_url = "netbox_tunnels"
     author = "Justin Drew"
     author_email = "***REMOVED***"
+    min_version = "2.8.3"
     required_settings = []
     # default_settings = {}
     # caching_config = {}
