ff8e4526c1b4dd521c5f9513eef0ffd8b3147677
Justin Drew <jdrew82@users.noreply.github.com>
2020-05-31 15:21:25 -0500

    Version bump to 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.exists("README.md"):
 
 setup(
     name="netbox-tunnels",
-    version="0.2.3",
+    version="0.3.0",
     description="A plugin for NetBox to support documentation of network tunneling protocols, ie IPsec, GRE, L2TP, etc.",
     long_description=readme,
     long_description_content_type="text/markdown",
